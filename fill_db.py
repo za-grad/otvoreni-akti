e6c5c89fd63a0328eecaba34f3ee123854b870e1
Dražen Lučanin <kermit666@gmail.com>
2017-08-31 11:57:33 +0000

    create tables too

--- fill_db.py
+++ fill_db.py
@@ -20,12 +20,12 @@ class BaseModel(Model):
         database = db
 
 class Category(BaseModel):
-    type = CharField()
+    type = CharField(unique=True)
     class Meta:
         db_table = 'cat_type'
 
 class Source(BaseModel):
-    ForeignKeyField(Category, related_name='type_id')
+    ForeignKeyField(Category, related_name='type_id', null=True)
     nr = IntegerField()
     year = IntegerField()
     week = IntegerField()
@@ -35,23 +35,30 @@ class Source(BaseModel):
         db_table = 'db_source'
 
 class Item(BaseModel):
-    source = ForeignKeyField(Source)
+    source = ForeignKeyField(Source, null=True)
     item_number = IntegerField()
     item_description = CharField()
-    subject = CharField()
+    subject = CharField(1000)
     unit = CharField()
     item_url = CharField()
+    class Meta:
+        db_table = 'db_item'
 
 class Act(BaseModel):
-    item = ForeignKeyField(Item)
-    act_number = IntegerField()
+    item = ForeignKeyField(Item, null=True)
+    act_number = IntegerField(null=True)
     type = CharField()
-    subject = CharField()
-    content_url = CharField()
-    content = BlobField()
+    subject = CharField(1000)
+    content_url = CharField(unique = True)
+    content = TextField()
     
     class Meta:
         db_table = 'db_act'
+
+try:
+    db.create_tables([Category, Source, Item, Act])
+except ProgrammingError:
+    db.rollback()
 
 # test = Act(subject='bla', content='blabla', content_url = 'http://web.zagreb.hr/sjednice/2013/Sjednice_2013.nsf/DRJ?OpenAgent&31.%20listopada%202016.%20-%204.studenog%202016')
 # test.save()
@@ -66,20 +73,30 @@ class Act(BaseModel):
 # for act in Act.select():
 #     print(act.subject, act.content, act.content_url)
 
+for type_description in ['akti_gradonacelnika', 'dnevni_red_skupstine', 'pitanja_odgovori', 'dnevni_red_radna_tijela']:
+    try:
+        new_type = Category(type=type_description)
+        new_type.save()
+    except IntegrityError:
+        db.rollback()
+
 import pickle
 
 with open('./skupstina2.pkl', 'rb') as f:
     subjects = pickle.load(f)
     for subject in subjects:
         for act in subject['details']['acts']:
-            print(act['title'], act['text'], act['url'])
+            # print(len(act['title']), len(act['text']), len(act['url']))
             try:
                 content = act['text']
                 # content = content.replace('\xB2', '2')
                 # print(content)
-                new_act = Act(subject=act['title'], content=content, content_url=act['url'])
+                new_act = Act(subject=act['title'], content=content, content_url=act['url'], type='')
                 new_act.save()
-            except IntegrityError:
+                print('wrote')
+            except IntegrityError as e:
+                print(e)
+                db.rollback()
                 continue
             
             

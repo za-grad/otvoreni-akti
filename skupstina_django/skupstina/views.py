2089858622094ffc9079753aa53c56d528882dcc
ranajaydas <ranajay@outlook.com>
2020-01-20 08:54:16 +0100

    Replicated basic search of previous Flask app

--- skupstina_django/skupstina/views.py
+++ skupstina_django/skupstina/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.db.models import Q
+from .models import Act
 
-# Create your views here.
+base_url = 'http://web.zagreb.hr/'
+
+
+def search_results_view(request, search_term):
+    query_set = Act.objects.filter(
+        Q(content__icontains=search_term)
+        | Q(subject__icontains=search_term)
+    ).distinct()
+    result = {'matches': [base_url + m.content_url for m in query_set]}
+    return JsonResponse(result)
--- skupstina_django/skupstina_django/urls.py
+++ skupstina_django/skupstina_django/urls.py
@@ -1,21 +1,8 @@
-"""skupstina_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from skupstina.views import search_results_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/<search_term>', search_results_view, name='search_results'),
 ]

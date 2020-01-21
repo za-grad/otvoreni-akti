d7a6e523f2d18ecacfe9d5b2f108b4f818022bd9
ranajaydas <ranajay@outlook.com>
2020-01-21 15:27:25 +0100

    Added 'AND' function to search and improved front-end

--- skupstina_django/skupstina/utils.py
+++ skupstina_django/skupstina/utils.py
@@ -1,4 +1,5 @@
-from django.db.models import Q
+from django.db.models import Q as QDjango
+from elasticsearch_dsl import Q
 from .documents import ActDocument, act_analyzer
 from .models import Act
 
@@ -6,21 +7,31 @@ base_url = 'http://web.zagreb.hr/'
 
 
 def sql_search(search_term):
+    """Returns result in JSON format"""
     query_set = Act.objects.filter(
-        Q(content__icontains=search_term)
-        | Q(subject__icontains=search_term)
+        QDjango(content__icontains=search_term)
+        | QDjango(subject__icontains=search_term)
     ).distinct()
-    result = {'matches': [base_url + m.content_url for m in query_set]}
-    return result
+    results = {'matches': [base_url + m.content_url for m in query_set]}
+    return results
 
 
 def elastic_search(search_term):
-    query_set = ActDocument.search().filter("match", content=search_term)
-    result = {'matches': [base_url + m.content_url for m in query_set]}
+    response = act_analyzer.simulate(search_term)
+    tokens = [t.token for t in response.tokens]
+    print(tokens)
 
-    # Check the token used for searching
-    # response = act_analyzer.simulate(search_term)
-    # tokens = [t.token for t in response.tokens]
-    # print(tokens)
+    if 'and' in tokens:
+        q = Q("multi_match", query=tokens[0], fields=['content', 'subject'])
+        for token in tokens:
+            if token != 'and':
+                q = q & Q("multi_match", query=token, fields=['content', 'subject'])
+    else:
+        q = Q("multi_match", query=search_term, fields=['content', 'subject'])
 
-    return result
+    query_set = ActDocument.search().query(q)
+
+    # Override Elasticsearch's default max of 10 results
+    results = query_set[0:query_set.count()].execute()
+
+    return results
--- skupstina_django/skupstina/views.py
+++ skupstina_django/skupstina/views.py
@@ -2,19 +2,25 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .utils import sql_search, elastic_search
 
+base_url = 'http://web.zagreb.hr'
+
 
 def sql_search_results_view(request):
     if request.GET:
         search_term = request.GET.get('q')
-        result = sql_search(search_term)
-        return JsonResponse(result)
+        results = sql_search(search_term)
+        return JsonResponse(results)
 
 
 def elastic_search_results_view(request):
     if request.GET:
         search_term = request.GET.get('q')
-        result = elastic_search(search_term)
-        return JsonResponse(result)
+        results = elastic_search(search_term)
+        context = {
+            'results': results,
+            'base_url': base_url,
+            }
+        return render(request, 'skupstina/results.html', context)
 
 
 def search_bar(request):

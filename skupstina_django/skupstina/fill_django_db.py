140c4ab13405d0da75749c500a11f4db72e02551
ranajaydas <ranajay@outlook.com>
2020-01-27 15:54:00 +0100

    Updated scraper to retry on errors and added all date ranges

--- skupstina_django/skupstina/fill_django_db.py
+++ skupstina_django/skupstina/fill_django_db.py
@@ -1,5 +1,14 @@
 import pickle
+import os
 from .models import Category, Source, Item, Act
+
+
+def find_pickles(scraper_path):
+    pickles = []
+    for file in os.listdir(scraper_path):
+        if file.endswith('.pkl'):
+            pickles.append(os.path.join(scraper_path, file))
+    return pickles
 
 
 def fill_django_db():
@@ -12,17 +21,29 @@ def fill_django_db():
             new_type = Category(type=type_description)
             new_type.save()
 
-    with open('../skupstina.pkl', 'rb') as f:
-        subjects = pickle.load(f)
-        for subject in subjects:
-            for act in subject['details']['acts']:
-                # Populate the Act table
-                content = act['text']
-                if not Act.objects.filter(content_url=act['url']).exists():
-                    new_act = Act(subject=act['title'], content=content, content_url=act['url'], type='')
-                    new_act.save()
+    found_pickles = find_pickles('../skupstina_scraper/')
 
-        print('wrote')
+    if found_pickles:
+        for found_pickle in found_pickles:
+            with open(found_pickle, 'rb') as f:
+                subjects = pickle.load(f)
+                print('Opened pickle file...')
+                for subject in subjects:
+                    for act in subject['details']['acts']:
+                        # Populate the Act table
+                        content = act['act_text']
+                        if not Act.objects.filter(content_url=act['act_url']).exists():
+                            print('Adding ', act['act_title'])
+                            new_act = Act(
+                                subject=act['act_title'],
+                                content=content,
+                                content_url=act['act_url'],
+                                type=''
+                            )
+                            new_act.save()
+            print('Added items from ', found_pickle)
+    else:
+        print('No new data found.')
 
 
 fill_django_db()
---
+++ skupstina_scraper/scrape_utils.py
@@ -0,0 +1,111 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+root_url = 'http://web.zagreb.hr'
+
+
+def parse_subjects_list(url):
+    site = requests.get(url).content
+    soup = BeautifulSoup(site, 'html.parser')
+    els = soup.select('.centralTD ul ol li')
+    print(len(els), ' elements')
+    subjects = []
+    for el in els:
+        subel = el.contents[0]
+        print(subel)
+        try:
+            link = subel.attrs['href']
+        except AttributeError:
+            continue
+        title = subel.select('b')[0].contents[0]
+        subjects.append({'title:': title, 'subject_url': root_url + link})
+    return subjects, len(els)
+
+
+def get_visible_text(soup):
+    # kill all script and style elements
+    if 'location.replace(' in soup.getText():
+        # Additional check for pages with JavaScript redirects
+        result = re.search('else\n {4}location.replace[(]"(.*)"[)];', soup.getText())
+        act_url = result.group(1)
+        site = requests.get(root_url + act_url).content
+        soup = BeautifulSoup(site, 'html.parser')
+        text = get_visible_text(soup)
+        return text
+    for script in soup(["script", "style"]):
+        script.extract()  # rip it out
+    text = soup.getText()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+
+def parse_subject_details(url):
+    site = requests.get(url).content
+    soup = BeautifulSoup(site, 'html.parser')
+
+    text = get_visible_text(soup)
+    subject_details = {'text': text}
+
+    act_titles = [el.get_text().strip() for el in soup.select('td a')]
+    act_urls = [el.attrs['href'] for el in soup.select('td a')]
+
+    acts = []
+    for i, act_url in enumerate(act_urls):
+        site = requests.get(root_url + act_url).content
+        soup = BeautifulSoup(site, 'html.parser')
+        act_text = get_visible_text(soup)
+        act_title = act_titles[i]
+        acts.append({'act_text': act_text, 'act_url': act_url, 'act_title': act_title})
+    subject_details['acts'] = acts
+    return subject_details
+
+
+def scrape_everything(url_suffix: str, akti_file: str):
+    all_subjects = []
+    periods_url = root_url + url_suffix
+    with open('scrapes_completed.txt', 'a', encoding='utf8') as f:
+        # Create a new file if not already created
+        pass
+    with open('data/' + akti_file, encoding='utf8') as periods_fd:
+        for act_period in list(periods_fd):
+            with open('scrapes_completed.txt', 'r+', encoding='utf8') as scrapes_completed:
+                if act_period not in scrapes_completed.read():
+                    parse_complete = False
+                    act_period = act_period.strip()
+                    print('\nScraping period: ', act_period)
+                    url = (periods_url + act_period).replace(' ', '%20')
+                    print(url)
+                    subjects, num_els = parse_subjects_list(url)
+                    all_subjects.extend(subjects)
+                    while not parse_complete:
+                        parse_complete = True
+                        for subject in subjects:
+                            if 'details' not in subject:
+                                try:
+                                    print('Parsing ', subject['subject_url'])
+                                    subject_details = parse_subject_details(subject['subject_url'])
+                                    subject['details'] = subject_details
+                                    print('Parsed  ', subject['subject_url'], ' **')
+                                except:
+                                    print('Error occurred in parsing ', subject['subject_url'])
+                                    parse_complete = False
+                    scrapes_completed.write('{}\n'.format(act_period))
+    print(all_subjects)
+    return all_subjects
+
+
+# def item_checker():
+#     act_url = '/sjednice/2017/Sjednice_2017.nsf/Dokument_opci_sjednica_noatt_web?OpenForm&ParentUNID=6591FDDD6E4FEF15C12584F00030C87E'
+#     # act_url = '/sjednice/2017/Sjednice_2017.nsf/Dokument_opci_sjednica_noatt_web?OpenForm&ParentUNID=6591FDDD6E4FEF15C12584F00030C87E&AutoFramed'
+#     site = requests.get(root_url + act_url).content
+#     print(root_url + act_url, '\n')
+#     soup = BeautifulSoup(site, 'html.parser')
+#     act_text = get_visible_text(soup)
+#     print('Act text:', act_text)
+# item_checker()

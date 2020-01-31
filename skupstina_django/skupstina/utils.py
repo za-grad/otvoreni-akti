28a1c084b65c99727e04a319242d40297c5263bc
ranajaydas <ranajay@outlook.com>
2020-01-31 18:16:52 +0100

    Added exact search terms feature

--- skupstina_django/skupstina/utils.py
+++ skupstina_django/skupstina/utils.py
@@ -1,4 +1,5 @@
 import dateparser
+import re
 from datetime import datetime, timedelta
 from elasticsearch_dsl import Q
 from .documents import ActDocument, act_analyzer
@@ -7,6 +8,29 @@ base_url = 'http://web.zagreb.hr/'
 
 
 def elastic_search(search_term, *args, **kwargs):
+    """
+    Search function using Elasticsearch.
+
+    Accepts the following kwargs:
+    'start_date' : Excludes any results before start_date
+    'end_date' : Excludes any results after end_date
+
+    Accepts the following keywords inside search_term:
+    'AND' : Use AND before search terms that must be included
+    'NOT' : Use NOT before search terms that must be excluded
+    'OR' : Use OR or space between search terms to include all terms
+    "" : Search terms inside quotation marks will be searched for an exact match
+    """
+    # Regex to search for exact terms within quotation marks
+    exact_search_terms = re.findall(r'"(.*?)"', search_term)
+    exact_terms_dict = {}
+    if exact_search_terms:
+        for i, exact_term in enumerate(exact_search_terms):
+            if exact_term:
+                exact_terms_dict['ex__' + str(i)] = exact_term
+                search_term = search_term.replace(exact_term, 'ex__' + str(i))
+
+    # Break the search term into individual tokens
     response = act_analyzer.simulate(search_term)
     tokens = [t.token for t in response.tokens]
     print(tokens)
@@ -15,20 +39,39 @@ def elastic_search(search_term, *args, **kwargs):
     start_date = dateparser.parse(kwargs['start_date'])
     end_date = dateparser.parse(kwargs['end_date'])
 
-    keywords = {'and', 'not'}
-    keywords_in_tokens = len(keywords.intersection(set(tokens))) > 0
-    if keywords_in_tokens:
-        q = Q("multi_match", query=tokens[0], fields=['content', 'title'])
-        for keyword_position, token in enumerate(tokens):
-            if token in keywords:
-                if keyword_position + 1 != len(tokens):
-                    new_query = Q("multi_match", query=tokens[keyword_position+1], fields=['content', 'title'])
-                    if token == 'and':
-                        q = q & new_query
-                    elif token == 'not':
-                        q = q & ~new_query
+    keywords = {'and', 'not', 'or'}
+    query_string = ''
+
+    # Create a string to be evaluated later
+    for position, token in enumerate(tokens):
+        if token in keywords and (position == 0 or position == len(tokens)-1):
+            # Ignore any keywords if they are the first or last search term
+            pass
+        elif token in exact_terms_dict:
+            query_string += """Q('query_string', query='"{}"', fields=['content', 'title']) |"""\
+                .format(exact_terms_dict[token])
+        elif token == 'and':
+            query_string = query_string[:-1] + "&"
+        elif token == 'or':
+            query_string = query_string[:-1] + "|"
+        elif token == 'not':
+            query_string = query_string[:-1] + "& ~"
+        else:
+            query_string += "Q('multi_match', query='{}', fields=['content', 'title']) |".format(token)
+
+    if query_string:
+        if query_string[-1] == '&' or query_string[-1] == '|':
+            # Pop the last character
+            query_string = query_string[:-1]
+        print(query_string)
+        try:
+            q = eval(query_string)
+        except SyntaxError as e:
+            print('Syntax error occurred in evaluating query:\n', e)
+            q = Q('multi_match', query='', fields=['content', 'title'])
     else:
-        q = Q("multi_match", query=search_term, fields=['content', 'title'])
+        # In case of no user input
+        q = Q('multi_match', query='', fields=['content', 'title'])
 
     query_set = ActDocument.search()\
         .query(q)\

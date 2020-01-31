ed3dcfca7283d0c95d24cd3633caa99f93d6fdd0
ranajaydas <ranajay@outlook.com>
2020-01-31 14:37:45 +0100

    Automatically scrape date ranges to .txt file

--- skupstina_django/scraper/scrape.py
+++ skupstina_django/scraper/scrape.py
@@ -1,4 +1,5 @@
-from .scrape_utils import scrape_everything
+from .scrape_utils_data import scrape_everything
+from .scrape_utils_dates import extract_dates
 
 # Dictionary linking each akti_*.txt file to its unique URL
 akti_dict = {
@@ -7,5 +8,19 @@ akti_dict = {
     'akti_2009-2013.txt': '/sjednice/Sjednice_2009.nsf/DRJ?OpenAgent&',
 }
 
+# Dictionary linking each Act database to its sidebar URL
+sidebar_dict = {
+    '2017-20xx': 'http://web.zagreb.hr/sjednice/2017/Sjednice_2017.nsf/web_pretraga?OpenForm&BaseTarget=desni',
+    '2013-2017': 'http://web.zagreb.hr/sjednice/2013/Sjednice_2013.nsf/web_pretraga?OpenForm&BaseTarget=desni',
+    '2009-2013': 'http://web.zagreb.hr/sjednice/Sjednice_2009.nsf/web_pretraga?OpenForm&BaseTarget=desni',
+}
+
+
+for k, v in sidebar_dict.items():
+    print('Searching for any new date ranges to be scraped from {}...'.format(k))
+    extract_dates(period=k, url=v)
+
+
 for k, v in akti_dict.items():
+    print('Scrape started for date ranges in {}'.format(k))
     subjects = scrape_everything(akti_file=k, url_suffix=v)
---
+++ skupstina_django/scraper/scrape_utils_dates.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract_months(year, url):
+    payload = {
+        '__Click': '$Refresh',
+        'SaveOptions': 0,
+        'server': 'szglotweb',
+        'trazi': '',
+        '% % Surrogate_rb_godina': 1,
+        'rb_godina': year,
+        '% % Surrogate_rb_sjednice': 1,
+    }
+    site = requests.post(url, data=payload).content
+    soup = BeautifulSoup(site, 'html.parser')
+    months_soup = soup.find("select", {'name': 'rb_sjednice'}).findAll('option')
+    month_list = months_soup[0].text.split('\n')
+    # Remove any empty items from month_list
+    month_list = list(filter(None, month_list))
+    return month_list
+
+
+def extract_dates(period, url):
+    with open('scraper/data/akti_' + period + '.txt', 'r+', encoding='utf8') as periods_fd:
+        months_on_file = periods_fd.read().splitlines()
+        site = requests.get(url).content
+        soup = BeautifulSoup(site, 'html.parser')
+        years_soup = soup.find("select", {'name': 'rb_godina'}).findAll('option')
+        # Get list of years in descending order
+        year_list = years_soup[0].text.split('\n')
+        year_list.sort(reverse=True)
+        for year in year_list:
+            month_list = extract_months(year, url)
+            for month in month_list:
+                if month not in months_on_file:
+                    print('Adding {} to {}'.format(month, periods_fd))
+                    periods_fd.write('{}\n'.format(month))
+

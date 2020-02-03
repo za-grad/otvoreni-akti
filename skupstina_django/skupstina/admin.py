1d075178b9666ecbf912bacf5d3bb60fc1233531
ranajaydas <ranajay@outlook.com>
2020-02-03 08:21:56 +0100

    Slight improvement to Django admin page

--- skupstina_django/skupstina/admin.py
+++ skupstina_django/skupstina/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-
 from .models import Period, Item, Subject, Act
 
-admin.site.register(Period)
 admin.site.register(Item)
-admin.site.register(Subject)
-admin.site.register(Act)
+
+
+@admin.register(Act)
+class ActAdmin(admin.ModelAdmin):
+    # List view
+    list_display = ('title', 'file_type', 'content_url',)
+    search_fields = ('title', 'file_type', 'content', 'content_url',)
+
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    # List view
+    list_display = ('period_text', 'start_date', 'end_date', 'period_url',)
+    search_fields = ('period_text', 'period_url',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    # List view
+    list_display = ('subject_title', 'item', 'subject_url',)
+    search_fields = ('subject_title', 'subject_url',)

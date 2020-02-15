62632cabde299f2d20b7dfca62cfa10b92631c4c
ranajaydas <ranajay@outlook.com>
2020-02-15 08:17:42 +0100

    Changed template check to response code check in test

--- otvoreni_akti/apps/search/tests/test_views.py
+++ otvoreni_akti/apps/search/tests/test_views.py
@@ -18,22 +18,23 @@ class TestSearchResults(TestCase):
     def test_can_handle_different_searches(self):
         # Search for 'Zagreb' may take a long time but shouldn't crash above 10,000 results
         response = self.client.get('/search/?q=zagreb')
-        self.assertTemplateUsed(response, 'search/search_results.html')
+        self.assertEqual(response.status_code, 200)
 
         # Tests for keyword searches with odd combinations
         response = self.client.get('/search/?q=%26%26notnotandand')
-        self.assertTemplateUsed(response, 'search/search_results.html')
+        self.assertEqual(response.status_code, 200)
 
         response = self.client.get('/search/?q=zagrebandandnotnotordubrovnik')
-        self.assertTemplateUsed(response, 'search/search_results.html')
+        self.assertEqual(response.status_code, 200)
 
+        # Tests for incorrect dates
         response = self.client.get(
             '/search/?q="imenovanju+%C4%8Dlana" not %C5%A0kolskog and predla%C5%BEe'
-            '&start_date=2019-10-14'
-            '&end_date=2099-02-29'
+            '&start_date=2019-10-32'
+            '&end_date=2099-02-30'
         )
-        self.assertTemplateUsed(response, 'search/search_results.html')
+        self.assertEqual(response.status_code, 200)
 
         # Tests for empty search
         response = self.client.get('/search/?q=')
-        self.assertTemplateUsed(response, 'search/search_results.html')
+        self.assertEqual(response.status_code, 200)

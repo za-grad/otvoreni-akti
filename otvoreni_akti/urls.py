74a458290d4bc70d948cb47453298b5f4397ad72
ranajaydas <ranajay@outlook.com>
2020-03-08 23:32:03 +0100

    Added an act_detail view to show archived version of the acts from database

--- otvoreni_akti/urls.py
+++ otvoreni_akti/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .apps.search.views import search_results, search_home
+from .apps.search.views import search_results, search_home, act_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', search_home, name='search_home'),
     path('search/', search_results, name='search_results'),
+    path('acts/<int:id>/', act_detail, name='act_detail'),
 ]

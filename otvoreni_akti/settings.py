4525861f5ed2afb7dfe7e40a7b1f4fecd447d009
ranajaydas <ranajay@outlook.com>
2020-06-12 16:03:09 +0800

    Changed date format to Croatian

--- otvoreni_akti/settings.py
+++ otvoreni_akti/settings.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'hr'
 # set your local time zone to more easily analyse data on the backend
 TIME_ZONE = 'CET'
 USE_I18N = True

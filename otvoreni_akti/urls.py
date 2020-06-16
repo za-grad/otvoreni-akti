77cf41404357823e1cfdac0d89e671c25f071113
Dražen Lučanin <kermit666@gmail.com>
2020-06-17 01:22:52 +0200

    add manifest, 'read more' small text fixes

--- otvoreni_akti/urls.py
+++ otvoreni_akti/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .apps.search.views import search_results, search_home, act_detail, view_404
+from .apps.search.views import search_results, search_home, act_detail, about, view_404
 
 handler404 = view_404
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', search_home, name='search_home'),
     path('search/', search_results, name='search_results'),
     path('acts/<int:id>/', act_detail, name='act_detail'),
+    path('manifest/', about, name='about'),
 ]
